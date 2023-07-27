677e6502903954b5450f5c8804dc457d36939c59
kkharel <kkharel@uci.edu>
2023-07-27 15:01:23 -0700

    Adding Matrix Multiplication and Sparse Matrix Multiplication

---
+++ Matrix_Multiplication.py
@@ -0,0 +1,65 @@
+# Matrix Multiplication
+
+def matrix_multiply(matrix_a, matrix_b):
+  
+  row_a, column_a = len(matrix_a), len(matrix_a[0])
+  row_b, column_b = len(matrix_b), len(matrix_b[0])
+  
+  if column_a != row_b:
+    raise ValueError("Matrix Dimension not compatible for multiplication")
+  
+  # result matrix
+  matrix_c = [[0*len(matrix_a) for i in range(column_b)] for j in range(row_a)]
+  
+  for i in range(row_a):
+    for j in range(column_b):
+      for k in range(column_a):
+        matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
+        
+  return matrix_c
+
+
+mat1 = [[1,2,3],[4,5,6]]
+mat2 = [[7,8],[9,10],[11,12]]
+
+matrix_multiply(mat1, mat2)
+     
+     
+# Sparse Matrix Multiplication -- (most of the entries in a matrix are zero)
+
+def sparse_matrix_multiply(matrix_a, matrix_b):
+  
+  row_a, column_a = len(matrix_a), len(matrix_a[0])
+  row_b, column_b = len(matrix_b), len(matrix_b[0])
+  
+  sparse_a = non_zero_dict(matrix_a)
+  sparse_b = non_zero_dict(matrix_b)
+  
+  if column_a != row_b:
+    raise ValueError("Matrix Multiplication is not valid")
+  
+  matrix_c = [[0*len(matrix_a) for i in range(column_b)] for j in range(row_a)]
+  
+  for i, k in sparse_a.keys():
+    for j in range(column_b):
+      if (k,j) in sparse_b.keys():
+        matrix_c[i][j] += sparse_a[(i,k)]*sparse_b[(k,j)]
+  return matrix_c
+  
+  
+  
+def non_zero_dict(matrix):
+  dictionary = {}
+  for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+      if matrix[i][j] != 0:
+        dictionary[(i,j)] = matrix[i][j]
+  return dictionary
+
+
+mat1 = [[1,0,0],[0,1,0],[1,0,0]]
+mat2 = [[1,2,2],[2,1,3],[-1,-2,-5]]
+
+sparse_matrix_multiply(mat1,mat2)
+
+    
