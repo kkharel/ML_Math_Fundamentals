5088c5b5cfa3f5d488bf61f72db441ab2a63cee0
kkharel <kkharel@uci.edu>
2023-07-27 15:22:18 -0700

    edge cases correction

--- Matrix_Multiplication.py
+++ Matrix_Multiplication.py
@@ -27,34 +27,32 @@ matrix_multiply(mat1, mat2)
      
 # Sparse Matrix Multiplication -- (most of the entries in a matrix are zero)
 
-def sparse_matrix_multiply(matrix_a, matrix_b):
-  
-  row_a, column_a = len(matrix_a), len(matrix_a[0])
-  row_b, column_b = len(matrix_b), len(matrix_b[0])
-  
-  sparse_a = non_zero_dict(matrix_a)
-  sparse_b = non_zero_dict(matrix_b)
-  
-  if column_a != row_b:
-    raise ValueError("Matrix Multiplication is not valid")
-  
-  matrix_c = [[0*len(matrix_a) for i in range(column_b)] for j in range(row_a)]
-  
-  for i, k in sparse_a.keys():
-    for j in range(column_b):
-      if (k,j) in sparse_b.keys():
-        matrix_c[i][j] += sparse_a[(i,k)]*sparse_b[(k,j)]
-  return matrix_c
-  
-  
-  
-def non_zero_dict(matrix):
-  dictionary = {}
-  for i in range(len(matrix)):
-    for j in range(len(matrix[0])):
-      if matrix[i][j] != 0:
-        dictionary[(i,j)] = matrix[i][j]
-  return dictionary
+def sparse_matrix_multiplication(matrix_a, matrix_b):
+    row_a, column_a = len(matrix_a), len(matrix_a[0])
+    row_b, column_b = len(matrix_b), len(matrix_b[0])
+
+    if column_a != row_b:
+        raise ValueError("Matrix Multiplication not valid")
+
+    sparse_a = get_dict(matrix_a)
+    sparse_b = get_dict(matrix_b)
+
+    matrix_c = [[0*len(matrix_b[0]) for _ in range(column_b)] for _ in range(row_a)]
+
+    for (i, k) in sparse_a:
+        for j in range(column_b):
+            if (k, j) in sparse_b:
+                matrix_c[i][j] += sparse_a[(i, k)] * sparse_b[(k, j)]
+
+    return matrix_c
+
+def get_dict(matrix):
+    dictionary = {}
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0:
+                dictionary[(i, j)] = matrix[i][j]
+    return dictionary
 
 
 mat1 = [[1,0,0],[0,1,0],[1,0,0]]
