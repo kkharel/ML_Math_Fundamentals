184516a04de998ae7ec7896be670a5dec09aeea1
kkharel <kkharel@uci.edu>
2023-07-27 18:49:52 -0700

    adding comments for readability

--- Matrix_Multiplication.py
+++ Matrix_Multiplication.py
@@ -28,23 +28,22 @@ matrix_multiply(mat1, mat2)
 # Sparse Matrix Multiplication -- (most of the entries in a matrix are zero)
 
 def sparse_matrix_multiplication(matrix_a, matrix_b):
-    if len(matrix_a[0]) != len(matrix_b):
+    if len(matrix_a[0]) != len(matrix_b): # not valid matrix multiplication
         return [[]]
 
-    matrix_c = []
+    matrix_c = [] # initialize empty matrix c
 
-    for i in range(len(matrix_a)):
-        arr = []
-        for j in range(len(matrix_b[0])):
-            arr.append(0)
-        matrix_c.append(arr)
+    for i in range(len(matrix_a)): # for row in matrix a
+        arr = [] # initialize empty arr
+        for j in range(len(matrix_b[0])): # for column in matrix b
+            arr.append(0) # append 0 to array for every column in matrix b
+        matrix_c.append(arr) # append array to matrix c for every row in matrix a
 
-    for i in range(len(matrix_c)):
-        for j in range(len(matrix_c[0])):
-            for k in range(len(matrix_b)):
-                if matrix_a[i][k] == 0 or matrix_b[k][j] == 0:
-                    continue
-                    
+    for i in range(len(matrix_c)): # for every row in matrix c
+        for j in range(len(matrix_c[0])): # for every column in matrix c
+            for k in range(len(matrix_b)): # used to perform dot product during matmul. Iterates over common dimension (number of columns) of matrix a and matrix b. k represents index of common dimension
+                if matrix_a[i][k] == 0 or matrix_b[k][j] == 0:  # check if either element in matrix_a or matrix_b is zero
+                    continue    
                 matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
         
     return matrix_c
@@ -63,4 +62,3 @@ def sparse_matrix_multiplication(matrix_a, matrix_b):
         return (np.dot(np.array(matrix_a), np.array(matrix_b))).tolist()
     except:
         return [[]]
-
