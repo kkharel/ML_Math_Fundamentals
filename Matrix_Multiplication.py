37ddbd92568a4f6f0ba6d91ba09f9da7b2c4d750
kkharel <kkharel@uci.edu>
2023-07-27 18:17:40 -0700

    complete edge cases

--- Matrix_Multiplication.py
+++ Matrix_Multiplication.py
@@ -28,31 +28,26 @@ matrix_multiply(mat1, mat2)
 # Sparse Matrix Multiplication -- (most of the entries in a matrix are zero)
 
 def sparse_matrix_multiplication(matrix_a, matrix_b):
-    row_a, column_a = len(matrix_a), len(matrix_a[0])
-    row_b, column_b = len(matrix_b), len(matrix_b[0])
+    if len(matrix_a[0]) != len(matrix_b):
+        return [[]]
 
-    if column_a != row_b:
-        raise ValueError("Matrix Multiplication not valid")
+    matrix_c = []
 
-    sparse_a = get_dict(matrix_a)
-    sparse_b = get_dict(matrix_b)
+    for i in range(len(matrix_a)):
+        arr = []
+        for j in range(len(matrix_b[0])):
+            arr.append(0)
+        matrix_c.append(arr)
 
-    matrix_c = [[0*len(matrix_b[0]) for _ in range(column_b)] for _ in range(row_a)]
-
-    for (i, k) in sparse_a:
-        for j in range(column_b):
-            if (k, j) in sparse_b:
-                matrix_c[i][j] += sparse_a[(i, k)] * sparse_b[(k, j)]
-
+    for i in range(len(matrix_c)):
+        for j in range(len(matrix_c[0])):
+            for k in range(len(matrix_b)):
+                if matrix_a[i][k] == 0 or matrix_b[k][j] == 0:
+                    continue
+                    
+                matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
+        
     return matrix_c
-
-def get_dict(matrix):
-    dictionary = {}
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] != 0:
-                dictionary[(i, j)] = matrix[i][j]
-    return dictionary
 
 
 
@@ -61,4 +56,11 @@ mat2 = [[1,2,2],[2,1,3],[-1,-2,-5]]
 
 sparse_matrix_multiply(mat1,mat2)
 
-    
+# using library
+import numpy as np
+def sparse_matrix_multiplication(matrix_a, matrix_b):
+    try:
+        return (np.dot(np.array(matrix_a), np.array(matrix_b))).tolist()
+    except:
+        return [[]]
+
